74cb1533d758cced422c8411a1cd51686da47060
ismaelash <contato@ismaelnascimento.com>
2022-04-12 23:04:52 -0300

    [UPDATED] version number hello word

--- src/index.ts
+++ src/index.ts
@@ -17,5 +17,5 @@ admin.initializeApp({
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase! #1");
+  response.send("Hello from Firebase! #2");
 });
